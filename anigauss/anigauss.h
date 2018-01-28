91f4cdce2cd4a6768e0d33bba200ec2579be99f7
cihanbilge <kayasa89@math.uh.edu>
2018-01-28 17:58:45 -0600

    Add files via upload

---
+++ anigauss/anigauss.h
@@ -0,0 +1,6 @@
+#ifndef anigauss_h
+#define anigauss_h
+
+#include <stdio.h>
+
+#endif /* anigauss_h */
---
+++ anigauss/anigauss_mex.h
@@ -0,0 +1,6 @@
+#ifndef anigauss_mex_h
+#define anigauss_mex_h
+
+#include <stdio.h>
+
+#endif /* anigauss_mex_h */
